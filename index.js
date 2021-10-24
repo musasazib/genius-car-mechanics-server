54cf26eceb33292b40af1db30631564f716a6f3a
musasazib <musasazib@gmail.com>
2021-10-25 03:54:32 +0600

    DB - Get API, Single get API, Post API, Delete API

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 const express = require('express');
-const app = express();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
 const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyv3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // Get API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // Get single API
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('Getting specific Id', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // Post API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('Hit the post API', service);
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        })
+
+        // Delete API
+        app.delete('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    }
+    finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Genius Car Mechanics Server');
